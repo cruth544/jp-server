df56d5dbb870cded26120d61460789e2c41651b2
Chad Rutherford <chad8708@c02rf10sg8wm.attlocal.net>
2017-04-09 00:07:11 -0700

    made rsync not -n

--- rsync.sh
+++ rsync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rsync -rave "ssh -i ../jp_wedding.pem" * ec2-54-202-204-95.us-west-2.compute.amazonaws.com:/home/ubuntu/wedding-server/ --exclude node_modules
+rsync -rave "ssh -i jp_wedding.pem" * ec2-54-202-204-95.us-west-2.compute.amazonaws.com:/home/ubuntu/wedding-server/ --exclude node_modules
 
